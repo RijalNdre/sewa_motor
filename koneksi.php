7b36a99e5ada993a713e067d65183f0971b622a9
Rizal Andrias <93868952+RijalNdre@users.noreply.github.com>
2023-05-12 08:22:12 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "motor");
+$koneksi = mysqli_connect("localhost", "root", "", "db_motor");
 if (mysqli_connect_errno()){
     echo "Koneksi Gagal" . mysqli_connect_error();
 }
